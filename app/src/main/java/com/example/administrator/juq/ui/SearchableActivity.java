fb8e7023bff2c615fec1257e4ca423b62413772e
Bi guang <biguangon@gmail.com>
2017-04-12 17:57:38 +0800

    Add search UI

--- app/src/main/java/com/example/administrator/juq/ui/SearchableActivity.java
+++ app/src/main/java/com/example/administrator/juq/ui/SearchableActivity.java
@@ -1,8 +1,23 @@
 package com.example.administrator.juq.ui;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.administrator.juq.R;
+
 /**
  * Created by Administrator on 2017/4/5.
  */
 
-public class SearchableActivity {
+public class SearchableActivity extends ListView {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.searchable);
+    }
+    public SearchableActivity(Context context) {
+        super(context);
+    }
 }
