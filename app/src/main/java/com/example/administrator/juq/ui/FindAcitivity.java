a04392eda382914cb3e5451b2a7f7cbb2ade9a39
Bi guang <biguangon@gmail.com>
2017-04-17 18:08:25 +0800

    Add search UI

--- app/src/main/java/com/example/administrator/juq/ui/FindAcitivity.java
+++ app/src/main/java/com/example/administrator/juq/ui/FindAcitivity.java
@@ -1,8 +1,17 @@
 package com.example.administrator.juq.ui;
 
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.animation.AnimationSet;
+import android.widget.LinearLayout;
+
+import com.example.administrator.juq.R;
+
+import java.io.LineNumberReader;
 
 /**
  * Created by Administrator on 2017/4/13.
@@ -12,5 +21,26 @@ public class FindAcitivity extends AppCompatActivity {
 
     private TabLayout mTableLayout;
     private Toolbar mToolbar;
+    private String[] TITLE = {"推荐", "房源", "体验", "攻略"};
+    private CollapsingToolbarLayout mToolbarLayout;
+    private AppBarLayout mAppBarLayout;
+    private CollapsingToolbarLayoutState mLayoutState = CollapsingToolbarLayoutState.EXPANDED;
+    private LinearLayout mLlContent;
+    private LinearLayout mLlTitle;
+    private AnimationSet mHideSet;
+    private AnimationSet mShowSet;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find);
+    }
+
+    private enum CollapsingToolbarLayoutState {
+        EXPANDED,
+        COLLAPSED,
+        INTERNEDSTATE
+    }
 
 }
