3847e8e603071e5f1602b949c500e9bc0ab129f8
walkermannum1 <biguangon@gmail.com>
2017-04-18 22:38:36 +0800

    add UI

--- app/src/main/java/com/example/administrator/juq/ui/FindAcitivity.java
+++ app/src/main/java/com/example/administrator/juq/ui/FindAcitivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.animation.AnimationSet;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.example.administrator.juq.R;
@@ -26,6 +27,7 @@ public class FindAcitivity extends AppCompatActivity {
     private AppBarLayout mAppBarLayout;
     private CollapsingToolbarLayoutState mLayoutState = CollapsingToolbarLayoutState.EXPANDED;
     private LinearLayout mLlContent;
+    private ImageView mIvArrow;
     private LinearLayout mLlTitle;
     private AnimationSet mHideSet;
     private AnimationSet mShowSet;
@@ -39,6 +41,8 @@ public class FindAcitivity extends AppCompatActivity {
         mToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
         mToolbar =(Toolbar) findViewById(R.id.toolbar);
         mTabLayout = (TabLayout) findViewById(R.id.tablayout);
+        mLlContent = (LinearLayout) findViewById(R.id.ll_content);
+
     }
 
     private enum CollapsingToolbarLayoutState {
